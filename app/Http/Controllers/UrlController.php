d3b48bd6c1713f3134ef5ded13fd325062e5c0e3
VK <belsynth@gmail.com>
2021-08-25 14:51:27 +0300

    Добавляет реализацию экшенов в контроллере для шага 2

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -2,22 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\View\View;
 
 class UrlController extends Controller
 {
-    public function index()
+    public function create(): View
     {
-        return view('urls.index');
+        return view('urls.create');
     }
 
-    public function store()
+    public function store(Request $request): RedirectResponse
     {
-        return;
+        $input = $request->input('url.name');
+        $validator = Validator::make($request->input(), [
+            'url.name' => ['required', 'url', 'max:255'],
+        ]);
+
+        if ($validator->fails()) {
+            flash("Некорректный URL: $input")->error();
+            return back()->withInput()->withErrors($validator);
+        }
+
+        $url = $this->normalizeUrl($input);
+
+        if ($this->isUrlExists($url)) {
+            flash('Страница уже существует')->info();
+            $id = DB::table('urls')->where('name', '=', $url)->value('id');
+
+            return redirect()->route('urls.show', ['id' => $id]);
+        }
+
+        $now = now();
+        $urlData = [
+            'name' => $url,
+            'created_at' => $now,
+            'updated_at' => $now
+        ];
+
+        $id = DB::table('urls')->insertGetId($urlData);
+        flash('Страница успешно добавлена')->success();
+
+        return redirect()->route('urls.show', ['id' => $id]);
     }
 
-    public function show()
+    public function index(): View
     {
-        return view('urls.show');
+        $urls = DB::table('urls')->paginate(2); // TODO: скорректировать количество элементов
+        return view('urls.index', ['urls' => $urls]);
+    }
+
+    public function show(int $id): View
+    {
+        $url = DB::table('urls')->find($id);
+        return view('urls.show', ['url' => $url]);
+    }
+
+    private function normalizeUrl(string $url): string
+    {
+        $urlData = parse_url($url);
+        $scheme = $urlData['scheme'];
+        $host = $urlData['host'];
+
+        return "$scheme://$host";
+    }
+
+    private function isUrlExists(string $name): bool
+    {
+        return DB::table('urls')->where('name', '=', $name)->exists();
     }
 }
