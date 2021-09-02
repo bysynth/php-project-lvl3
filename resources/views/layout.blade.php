fecde200a99461d357dd7924d17e2e1761ee1111
VK <belsynth@gmail.com>
2021-09-02 12:33:30 +0300

    Меняет вью после переименования машртутов

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,7 @@
 <body class="min-vh-100 d-flex flex-column">
 <header class="flex-shrink-0">
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="{{ route('urls.create') }}">Анализатор страниц</a>
+        <a class="navbar-brand" href="{{ route('index') }}">Анализатор страниц</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -18,8 +18,8 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('urls.create') ? 'active' : '' }}"
-                       href="{{ route('urls.create') }}">Главная</a>
+                    <a class="nav-link {{ request()->routeIs('index') ? 'active' : '' }}"
+                       href="{{ route('index') }}">Главная</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link {{ request()->routeIs(['urls.index', 'urls.show']) ? 'active' : '' }}"
